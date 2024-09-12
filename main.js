d3f969fa714d644131efdd30472daffded31e474
Strider2342 <arpad.ivany@gmail.com>
2024-09-12 14:03:49 +0200

    init and render function added; updateCursorPosirtion function added; enter adds new line

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import fs from 'fs';
 import readline from 'readline';
+import ansi from 'ansi-escapes';
 
 const { stdin, stdout } = process;
 
 const filename = process.argv[2];
 
-let textContent = '';
+const cursorPosition = { x: 0, y: 0 };
+
+let contentBuffer = [''];
 
 if (!filename) {
   console.error('Usage: node main.js <filename>');
@@ -13,25 +16,58 @@ if (!filename) {
 }
 
 const save = () => {
-  fs.writeFileSync(filename, textContent);
+  fs.writeFileSync(filename, contentBuffer.join(''));
 };
+
+const updateCursorPosition = () => {
+  stdout.write(ansi.cursorHide);
+  stdout.write(ansi.cursorTo(cursorPosition.x, cursorPosition.y));
+  stdout.write(ansi.cursorShow);
+};
+
+const render = () => {
+  contentBuffer.forEach((line) => {
+    stdout.write(line);
+  });
+  stdout.write(ansi.cursorShow);
+};
+
+const init = () => {
+  cursorPosition.x = 0;
+  cursorPosition.y = 0;
+  stdout.write(ansi.clearScreen);
+  updateCursorPosition();
+  render();
+}
+
+init();
 
 stdin.setRawMode(true);
 readline.emitKeypressEvents(stdin);
 
 stdin.on('keypress', (str, key) => {
-  if (str) {
-    textContent += str;
+  if (key.ctrl) {
+    // save
+    if (key.ctrl && key.name === 's') {
+      save();
+    }
+
+    // exit
+    if (key.ctrl && key.name === 'c') {
+      stdout.write(ansi.clearScreen);
+      process.exit();
+    }
+  } else if (str) {
+    contentBuffer[cursorPosition.y] += str;
     stdout.write(str);
+    cursorPosition.x = Math.min(cursorPosition.x + 1, stdout.columns);
+    updateCursorPosition();
   }
 
-  // save
-  if (key.ctrl && key.name === 's') {
-    save();
-  }
-
-  // exit
-  if (key.ctrl && key.name === 'c') {
-    process.exit();
+  if (key.name === 'return') {
+    contentBuffer.push('');
+    cursorPosition.y = Math.min(cursorPosition.y + 1, stdout.rows);
+    cursorPosition.x = 0;
+    updateCursorPosition();
   }
 });
